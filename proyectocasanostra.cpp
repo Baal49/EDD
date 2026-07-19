3d747665c0b29a1f4a438130cd4e7c47701bee22
Reinaldo Belmonte <reinaldobelmonte9@gmail.com>
2026-07-18 21:38:18 -0400

    feat: se lee el csv y se inserta en el arbol

--- proyectocasanostra.cpp
+++ proyectocasanostra.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 using namespace std;
 template <typename N>
 class nodo{
     public:
         int id=0;
-        char nombre[100];
-        string apellido[100];
+        string nombre;
+        string apellido;
+        string genero;
         int edad=0;
         int idjefe=0;
         bool muelto=0;
@@ -16,7 +18,7 @@ class nodo{
         bool esjefe=0;
         nodo* hijoder=nullptr;
         nodo*  hijoizq=nullptr;
-    nodo(int id,string nombre,string apellido,int age,int idboss,bool dead,bool jail,bool wasboos,bool isboss){
+    nodo(int id,string nombre,string apellido,string genero,int age,int idboss,bool dead,bool jail,bool wasboos,bool isboss){
         this->id=id;
         this->nombre=nombre;
         this->apellido=apellido;
@@ -32,13 +34,16 @@ template <typename C>
 class Arbol{
     public:
     nodo<C>* jefe=nullptr;
-    void insertar(int id,string nombre,string apellido,int age,int idboss,bool dead,bool jail,bool wasboos,bool isboss){
+    int nivel=0;
+    void insertar(int id,string nombre,string apellido,string genero,int age,int idboss,bool dead,bool jail,bool wasboos,bool isboss){
+        cout<<"isboss: "<<isboss<<"dead: "<<dead<<endl;
         if(this->jefe==nullptr&&isboss==1&&dead==0){
-            nodo<C>* nodito=new nodo(dato);
+            nodo<C>* nodito=new nodo<C>(id,nombre,apellido,genero,age,idboss,dead,jail,wasboos,isboss);
             return;
         }
+        else{
         nodo<C>* padre=nullptr;
-        nodo<C>* act=this->root;
+        nodo<C>* act=this->jefe;
         while (act!=nullptr&&act->id!=id)
         {
             padre=act;
@@ -52,19 +57,45 @@ class Arbol{
         if(act!=nullptr){
             return;
         }
-        nodo<C>* nodito=new nodo(dato);
+        nodo<C>* nodito=new nodo<C>(id,nombre,apellido,genero,age,idboss,dead,jail,wasboos,isboss);
         if(padre->hijoizq==nullptr){
             padre->hijoizq=nodito;
+            nivel++;
         }
         else{
             padre->hijoder=nodito;
+            nivel++;
         }
         
     }
+}
+void mostrardato(){
+    while(){
+
+    }
+}
 };
 int main(){
  Arbol<int> arbolito;
  ifstream archivo("casanostra.csv");
- 
- arbolito.insertar();
+ vector<vector<string>> datos;
+ string linea;
+ std::string encabezado;
+ std::getline(archivo, encabezado); 
+ while(getline(archivo,linea)){
+    vector<string>fila;
+    stringstream ss(linea);
+    string celda;
+    while(getline(ss,celda,',')){
+        fila.push_back(celda);
+    }
+    datos.push_back(fila);
+ }
+ for (const auto& fila : datos) {
+        for (const auto& celda : fila) {
+            std::cout << celda << "\t";
+        }
+        std::cout << std::endl;
+    }
+ arbolito.insertar(stoi(datos[0][0]),datos[0][1],datos[0][2],datos[0][3],(stoi(datos[0][4])),(stoi(datos[0][5])!=0),(stoi(datos[0][6])!=0),(stoi(datos[0][7])!=0),(stoi(datos[0][8])!=0),(stoi(datos[0][9])!=0));
 }
